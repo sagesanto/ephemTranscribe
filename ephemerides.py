2953837043512335d4c12a1a14a5d4ac4fca8446
sagesanto <chellster1003@gmail.com>
2022-10-03 23:13:11 -0700

    committing assorted changes made locally on Maeby by Sage. additionally, adding a function to look for and correct a very specific error (see replace)

--- ephemerides.py
+++ ephemerides.py
@@ -1,10 +1,25 @@
-#program assumes n arguments, all of which are the names of objects, exactly as they appear on the ephemerides webpage, in the exact order they appear on the ephemerides webpage
-#assume downloaded text file of a ephemerides page titled "ephems.txt" in root directory
+# Sage Santomenna, Pomona, 2022
+# program assumes n arguments, all of which are the names of objects, exactly as they appear on the ephemerides webpage, in the exact order they appear on the ephemerides webpage
+# assume downloaded text file of a ephemerides page titled "ephems.txt" in root directory
 
-#separate out by name: identify the relevant text for each and write each to own text file in subfolder
-#then invoke awk command to transform each to telescope readable format in subfolder
+# separate out by name: identify the relevant text for each and write each to own text file in subfolder
+# then invoke awk command to transform each to telescope readable format in subfolder
 import sys
 import os
+import fileinput
+import re
+
+
+# this little function looks for and attempts to fix an issue that occurred on 2022 09 28 with target C835WW2 where MPC_scheduler_script_1.awk
+# failed to handle an overflow in dRA that led RA=0.087 (three decimals) and Dec = -10.97 to be interpreted as dRA = 0.087-10.97
+def replace(path):
+    with open(path, 'r') as file:
+        filedata = file.read()
+    filedata = (re.sub('\S\-', lambda m: m.group(0).replace('-', '') + ' -', filedata))
+    with open(path, 'w') as file:
+        file.write(filedata)
+
+
 if len(sys.argv) == 1:
     raise Exception("Program expects the number of objects as an argument")
 numObjects = sys.argv[1]
@@ -14,6 +29,7 @@ with open('ephems.txt') as f:
     for i in range(int(numObjects)):
         l = f.readline()
         filenames.append(l.replace('\n', ''))
+        print("found name " + l.replace('\n', ''))
         w = open('ephemeridesDir/' + filenames[-1] + '_ephem.txt', 'w')
         w.write(l)
         while True:
@@ -21,8 +37,8 @@ with open('ephems.txt') as f:
             if l == " \n":
                 w.close()
                 break
-            w.write(l) #put this after line 14 if the file needs to end with the whitespace
+            w.write(l)  # put this after line 14 if the file needs to end with the whitespace
 for name in filenames:
-    os.system("awk -f MPC_scheduler_script_1.awk ephemeridesDir/" + name + "_ephem.txt > /ephemeridesDir" + name + "_scheduler.txt")
-# probably wont be able to find the files because they're in the ephemeridesDir directory
-#  -> needs to be fixed when I can test on a linux vbox
+    replace('ephemeridesDir/' + name + "_ephem.txt")
+    os.system(
+        "awk -f MPC_scheduler_script_1.awk ephemeridesDir/" + name + "_ephem.txt > /ephemeridesDir" + name + "_scheduler.txt")
